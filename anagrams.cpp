7b3b006829b5430ee042908e38c02cc5412b37de
Adrien Dubois <adrien-dubois@outlook.com>
2019-03-15 19:47:57 +0100

    filename

---
+++ anagrams.cpp
@@ -0,0 +1,12 @@
+#include "anagrams.hpp"
+
+using std::string;
+
+Dictionary create_dictionary(const std::string& filename){
+
+}
+
+std::vector<std::vector<std::string>> 
+anagrams(const std::string& input, const Dictionary& dict, unsigned max){
+
+}
